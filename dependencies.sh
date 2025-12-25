c0817e774c3558561c27c04a63dc7670946cddd9
rajajisai <sairajaji1999@gmail.com>
2025-12-25 17:46:52 -0500

    reset defualt neo4j password

--- dependencies.sh
+++ dependencies.sh
@@ -5,12 +5,14 @@ docker run \
 -p 3306:3306 \
 -d mariadb:latest
 
-docker run -d \                                                                                               
-    --name neo4j \                     
+docker run -d \
+    --name neo4j \
     -p 7474:7474 -p 7687:7687 \
     -v ~/neo4j/data:/data \
     -v ~/neo4j/logs:/logs \
     -v ~/neo4j/plugins:/plugins \
+    #Have to neo4j password has to be set and different from default  password"neo4j"
+    -e NEO4J_AUTH=neo4j/armchair \ 
     neo4j:5.13
 
 docker run -d -p 6333:6333 -p 6334:6334 \
